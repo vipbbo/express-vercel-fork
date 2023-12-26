0465ea1e945e87a571ac473bde309cc306fcc647
paidaxing <32479732@qq.com>
2023-12-26 23:06:48 +0800

    测试

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 // 通过命令 npm i sha1 安装 sha1
 var sha1 = require('sha1');
 var sign = require('../utils/sign')
+
+var axios = require('axios')
+var {appid, secret} = require('../config')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -32,5 +35,22 @@ router.get('/jsapi', async function(req, res){
   console.log('conf', conf);
   res.send(conf);
 })
+
+
+
+// 生成签名
+router.get('/ticket', async function(req, res){
+    let tokenUrl = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=$${secret}`
+    let token_data = await axios.get(tokenUrl);
+    console.log('token_data', token_data);
+
+    let access_token = token_data.data.access_token; // 获取access_token
+    // 参考官方文档：https://developers.weixin.qq.com/doc/offiaccount/OA_Web_Apps/JS-SDK.html#62
+    let ticketUrl = `https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=${access_token}&type=jsapi`
+    let ticket_data = await axios.get(ticketUrl); // 获取jsapi的ticket
+    console.log('ticket_data', ticket_data);
+})
+
+
 
 module.exports = router;
