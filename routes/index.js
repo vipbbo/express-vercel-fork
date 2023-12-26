3d1b2866d4e7ac31bed4ed4f4b7879ae012367aa
zhiwang <zhibo.b.wang@capgemini.com>
2023-12-26 14:13:18 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* weixin-鉴权接口 */
+router.get('/wx-auth', function(req, res, next) {
+  res.send('Welcome to wx-auth!!!!');
+});
+
 module.exports = router;
