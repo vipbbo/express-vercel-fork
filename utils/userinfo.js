fb2b0b97320c89f152dc97e32bf729f03ef11639
paidaxing <32479732@qq.com>
2023-12-30 04:24:35 +0800

    updata acctoken url

--- utils/userinfo.js
+++ utils/userinfo.js
@@ -1,10 +1,10 @@
 var axios = require('axios')
+var {appid, secret} = require('../config')
 
 // 通过code换取网页授权access_token
 var userAccessToken = async function userAccessToken(code) {
     // 替换以下链接中的参数为实际值
-    var appId = 'wx6361298f0e180aa1';
-    var access_token_url = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appId}&secret=SECRET&code=${code}&grant_type=authorization_code`;
+    var access_token_url = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appId}&secret=${secret}&code=${code}&grant_type=authorization_code`;
     let access_token_data = await axios.get(access_token_url);
     let access_token = access_token_data.access_token;
     let openid = access_token_data.openid;
