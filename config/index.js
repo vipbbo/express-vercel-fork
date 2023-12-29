3f4fedf8797784918c0946781b0d135047c38f42
codingpaidaxing <122680363+codingpaidaxing@users.noreply.github.com>
2023-12-30 01:00:04 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    appid: 'wxa89dec44ecc473ea',
-    secret: '042c10da2e8f5b698d61515ed28cd236'
+    appid: 'wx6361298f0e180aa1',
+    secret: '741d6da7a14325fa0f49bb07fc8ae41b'
 }
