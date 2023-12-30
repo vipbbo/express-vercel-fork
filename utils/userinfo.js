8f104a76652e44979660cc7af41db7d6da44166b
paidaxing <32479732@qq.com>
2023-12-30 22:23:31 +0800

    修改获取用户基本信息

--- utils/userinfo.js
+++ utils/userinfo.js
@@ -20,6 +20,7 @@ var userInfo = async function userInfo(access_token, openid) {
     console.log(access_token,openid)
     let user_info_url = `https://api.weixin.qq.com/sns/userinfo?access_token=${access_token}&openid=${openid}&lang=zh_CN`
     // let user_info_url = `https://api.weixin.qq.com/cgi-bin/user/info?access_token=${access_token}&openid=${openid}&lang=zh_CN`;
+    console.log("获取用户的请求地址:", user_info_url);
     let result = axios.get(user_info_url);
     return result;
 }
