e37cfac940526ce619e956c0a8821d3cf4a57637
paidaxing <32479732@qq.com>
2024-01-04 00:49:15 +0800

    update url

--- web/src/api/wx-index.ts
+++ web/src/api/wx-index.ts
@@ -3,7 +3,7 @@ import { post } from '@/utils/request'
 export function fetchWxUser<T>() {
   console.log("============H5调用微信获取个人信息接口================")
   return post<T>({
-    url: 'https://service.ibitly.cn//users/wx-user',
+    url: 'https://service.ibitly.cn/users/wx-user',
   })
 }
 
