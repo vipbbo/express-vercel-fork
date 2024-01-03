406af55e83ccfb29ce094a12cb8de8b33d00b1de
paidaxing <32479732@qq.com>
2024-01-04 00:33:10 +0800

    update fetchWxUser

--- web/src/api/wx-index.ts
+++ web/src/api/wx-index.ts
@@ -3,7 +3,7 @@ import { post } from '@/utils/request'
 export function fetchWxUser<T>() {
   console.log("============H5调用微信获取个人信息接口================")
   return post<T>({
-    url: 'users/wx-user',
+    url: '/users/wx-user',
   })
 }
 
