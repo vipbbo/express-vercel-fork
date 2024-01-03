f0cb7e218741e17e30b3079c5b6297fb21a2af6c
paidaxing <32479732@qq.com>
2024-01-04 00:58:56 +0800

    配置跨域(npm i --save-dev @types/cors)

--- service/src/app.ts
+++ service/src/app.ts
@@ -1,6 +1,7 @@
 // src/app.ts
 import express, { Request, Response } from 'express';
 import { isNotEmptyString } from './utils/is'
+import cors from 'cors';
 import usersRouter from './routes/users';
 
 
@@ -11,6 +12,8 @@ const port = 3002;
 
 // app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// 使用 CORS 中间件
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript Express!');
